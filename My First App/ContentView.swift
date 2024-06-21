510314cf42ba6f33b28d921459dc4dd33f40b49c
Helena Lima <helena.limaa148@gmail.com>
2024-06-21 14:22:24 -0300

    Initial Commit

---
+++ My First App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My First App
+//
+//  Created by Isabel Pereira Lima on 20/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+        
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My First App/My_First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_AppApp.swift
+//  My First App
+//
+//  Created by Isabel Pereira Lima on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct My_First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
